88054f3783bd3308357ac6dfb23eac19b4b95f13
mintech-dot <75437079+mintech-dot@users.noreply.github.com>
2021-07-17 14:58:36 +0100

    Add files via upload

---
+++ TheEncrypter/EncryptionDecryption/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptionDecryption
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string hash = "M[%&e(@";
+        private void Encrypt_Click(object sender, EventArgs e)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(valuetxt.Text);
+            using(MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider() )
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using(TripleDESCryptoServiceProvider tripdes = new TripleDESCryptoServiceProvider() {  Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.ISO10126 })
+                {
+                    ICryptoTransform transform = tripdes.CreateEncryptor();
+                    byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+                    encrypttxt.Text = Convert.ToBase64String(result, 0, result.Length);
+                }
+            }
+        }
+
+        private void Decrypt_Click(object sender, EventArgs e)
+        {
+            byte[] data = Convert.FromBase64String(encrypttxt.Text);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider tripdes = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.ISO10126 })
+                {
+                    ICryptoTransform transform = tripdes.CreateDecryptor();
+                    byte[] result = transform.TransformFinalBlock(data, 0, data.Length);
+                    decrypttxt.Text = UTF8Encoding.UTF8.GetString(result);
+                }
+            }
+        }
+
+        private void aboutinfo_Click(object sender, EventArgs e)
+        {
+            about ab = new about();
+            ab.ShowDialog();
+        }
+    }
+}
---
+++ TheEncrypter/EncryptionDecryption/about.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace EncryptionDecryption
+{
+    partial class about
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(about));
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(23, 82);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(286, 126);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "My nickname is MinTech\r\n\r\nTelegram:   https://t.me/Sobhi_raid\r\n\r\nGithub:        h" +
+    "ttps://github.com/mintech-dot\r\n\r\nEmail:          mintech@protonmail.com";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(116, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "👤 Raid Sobhi";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Author :";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(311, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(221, 188);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // about
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(545, 252);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "about";
+            this.Style = MetroFramework.MetroColorStyle.Silver;
+            this.Load += new System.EventHandler(this.about_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
